354ec14d9cd0894fbac2f61b3f3ece61bc5ed26c
Tori Lew <torirlew@gmail.com>
2023-04-04 17:12:35 -0700

    Using spacing with views

--- Chapter03/ContentView.swift
+++ Chapter03/ContentView.swift
@@ -10,10 +10,20 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            //How is the order of the modifiers represented in XCode's Inspector panel?
+                //The modifiers are applied in order, so if the padding is applied before the color then the padding will be colored
+                //In the two Text views that are colored, one is applied before the padding (globe). The other is colored after the padding (pidgeon)
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
+                .background(Color.teal).padding()
+            Spacer()
             Text("Hello, world!")
+            Spacer()
+            Spacer()
+            Text("Pidgeon").padding(28)
+                .background(Color.teal)
+            Spacer()
             ZStack {
                 Text ("A")
                     .font(.system(size: 40))
@@ -21,6 +31,19 @@ struct ContentView: View {
                     .offset(x: 2, y: 2)
                 Text ("A")
                     .font(.system(size: 40))
+                Image(systemName: "house").offset(x:15,y:12).imageScale(.large).foregroundColor(Color.indigo)
+            }
+            HStack(alignment: .bottom){
+                Text ("Get").font(.title)
+                Text ("A")
+                Text ("Cat")
+            }
+            HStack(alignment: .top){
+                Text ("Get").font(.title)
+                Spacer()
+                Text ("A")
+                Spacer()
+                Text ("Cat")
             }
         }
         .padding()
